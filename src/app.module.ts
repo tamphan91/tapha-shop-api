c63c8c1c41164253df6a7a40e12d56221bab03cb
tampc <tamphanpw1@gmail.com>
2019-08-07 09:37:15 +0700

    modified db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,19 @@ import { PhotoModule } from './photo/photo.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'ec2-174-129-227-128.compute-1.amazonaws.com',
+      port: 5432,
+      username: 'dcnemggtczuvnc',
+      password: 'dd0f568d1aa0f976d1b206a8c1597c722c1d77bb492dc8fe95a9e5722378a480',
+      database: 'd7meov6tdkbfae',
+      // entities: ['./src/**/*.entity.ts", "./dist/**/*.entity.ts'],
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+      logging: true,
+      ssl: true,
+    }),
     AuthModule, UsersModule, PhotoModule],
   controllers: [AppController],
   providers: [AppService],
