2bf09b6f34e80b23ed6b4765bdafe3b9f50fadf7
tampc <tamphanpw1@gmail.com>
2019-10-18 09:39:29 +0700

    added crawl nike data

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Controller, Get, Param, Res, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+import { readdirSync, ensureDirSync, readJsonSync } from 'fs-extra';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   getHello(): string {
@@ -12,6 +13,27 @@ export class AppController {
 
   @Get('photos/:fileId')
   async serveAvatar(@Param('fileId') fileId, @Res() res): Promise<any> {
-    res.sendFile(fileId, { root: 'photos'});
+    res.sendFile(fileId, { root: 'photos' });
+  }
+
+  @Get('nike/sale')
+  async serveSaleNike(@Res() res, @Query('name') withName: string): Promise<any> {
+    const path = process.env.NIKE_SALE_PATH;
+    ensureDirSync(path);
+    const directories = readdirSync(path);
+    // tslint:disable-next-line:no-console
+    console.log('directories', directories);
+    if (directories.length === 0) {
+      res.send([]);
+    } else {
+      // tslint:disable-next-line:no-console
+      console.log('directories[directories.length - 1]', directories[directories.length - 1]);
+      // res.sendFile(directories[directories.length - 1], { root: path });
+      const products = readJsonSync(path + '/' + directories[directories.length - 1]);
+      const filterBy = str => products.filter(
+        item => new RegExp('^' + str.replace(/\*/g, '.*') + '$').test(item.name.toLowerCase()),
+      );
+      res.send(withName ? filterBy(`*${withName.toLowerCase()}*`) : products);
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -21,18 +21,21 @@ import { StockModule } from './stock/stock.module';
 import { OrderModule } from './order/order.module';
 import { OrderDetailModule } from './order_detail/orderDetail.module';
 import { InvoiceModule } from './invoice/invoice.module';
+import { scheduleJob } from 'node-schedule';
+import { launch } from 'puppeteer';
+import { writeJSON, readdirSync, remove, ensureDirSync } from 'fs-extra';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   const port = process.env.PORT || 3000;
   const userRepository = getRepository(User);
-  const adminUser = await userRepository.findOne({email: 'tamphan91@gmail.com'});
+  const adminUser = await userRepository.findOne({ email: 'tamphan91@gmail.com' });
   if (!adminUser) {
     const profileRepository = getRepository(Profile);
     // tslint:disable-next-line:max-line-length
-    const adminProfileToSave = profileRepository.create({firstName: 'tam', lastName: 'phan', gender: Gender.Male, dateOfBirth: '1991-10-04', roles: [UserRole.Admin, UserRole.Moderator, UserRole.User]});
+    const adminProfileToSave = profileRepository.create({ firstName: 'tam', lastName: 'phan', gender: Gender.Male, dateOfBirth: '1991-10-04', roles: [UserRole.Admin, UserRole.Moderator, UserRole.User] });
     const adminProfileReturn = await profileRepository.save(adminProfileToSave);
-    const adminUserToSave  = userRepository.create({email: 'tamphan91@gmail.com', password: '123', profileId: adminProfileReturn.id});
+    const adminUserToSave = userRepository.create({ email: 'tamphan91@gmail.com', password: '123', profileId: adminProfileReturn.id });
     await userRepository.save(adminUserToSave);
     Logger.log('Inited admin user successfull!');
   }
@@ -41,6 +44,84 @@ async function bootstrap() {
   // console.log('originalPrice: ', stock.productDetail.product.originalPrice);
   // // tslint:disable-next-line:no-console
   // console.log('discountPercent: ', stock.productDetail.discountPercent);
+
+  let times = 1;
+  scheduleJob('* * * * *', () => {
+    const atTimes = times++;
+    Logger.log('Start crawl Nike Sale Job at ' + new Date() + ` - ${atTimes}`);
+    launch().then(async browser => {
+      const page = await browser.newPage();
+      await page.goto('https://nike.com/us');
+
+      // click to show menu bar
+      await page.waitFor('#MobileMenuToggle');
+      await page.click('#MobileMenuToggle');
+
+      // click Men button
+      // tslint:disable-next-line:max-line-length
+      await page.waitFor('#gen-nav-commerce-header > header > nav.ncss-container.bg-white > section.d-sm-b > div > div.l-mobile-nav.d-lg-h.pt2-sm.pb2-sm > nav > div > div.mobile.menu-panel > ul > li:nth-child(3)');
+      // tslint:disable-next-line:max-line-length
+      await page.click('#gen-nav-commerce-header > header > nav.ncss-container.bg-white > section.d-sm-b > div > div.l-mobile-nav.d-lg-h.pt2-sm.pb2-sm > nav > div > div.mobile.menu-panel > ul > li:nth-child(3)');
+
+      // get href from Sale button
+      // tslint:disable-next-line:max-line-length
+      const salePageHref = await page.$eval('#gen-nav-commerce-header > header > nav.ncss-container.bg-white > section.d-sm-b > div > div.l-mobile-nav.d-lg-h.pt2-sm.pb2-sm > nav > div > div.mobile-menu-panel.is-active > ul > li:nth-child(6) > a', el => el.getAttribute('href'));
+      await page.goto(salePageHref);
+      await page.waitFor('#Wall > div > div.header-position.css-iqr4dm');
+
+      // tslint:disable-next-line:max-line-length
+      await page.waitFor('#Wall > div > div.categories.css-mzf2z2.is--mobile > div > div.simplebar-wrapper > div.simplebar-mask > div > div > a:nth-child(1) > div');
+      // tslint:disable-next-line:max-line-length
+      const countDiv = await page.$eval('#Wall > div > div.categories.css-mzf2z2.is--mobile > div > div.simplebar-wrapper > div.simplebar-mask > div > div > a:nth-child(1) > div', el => el.innerHTML);
+      const total = countDiv.replace(/\(|\)/gi, '');
+      // tslint:disable-next-line:max-line-length
+      await page.click('#Wall > div > div.categories.css-mzf2z2.is--mobile > div > div.simplebar-wrapper > div.simplebar-mask > div > div > a:nth-child(1)');
+
+      let productCards;
+      do {
+        Logger.log('start', total);
+        await page.evaluate(async () => {
+          await new Promise((resolve, reject) => {
+            let totalHeight = document.body.scrollHeight - 6000;
+            const distance = 100;
+            const timer = setInterval(() => {
+              const scrollHeight = document.body.scrollHeight;
+              window.scrollBy(0, distance);
+              totalHeight += distance;
+
+              if (totalHeight >= scrollHeight) {
+                clearInterval(timer);
+                resolve();
+              }
+            }, 100);
+          });
+        });
+        productCards = await page.$$('#Wall > div > div.results__body > div > main > section > div > div > .product-card__body');
+        Logger.log('end', productCards.length);
+      } while (productCards.length < total);
+      const products = [];
+      for (const productCard of productCards) {
+        const name = await productCard.$eval('.product-card__link-overlay', el => el.innerHTML);
+        const priceReduced = await productCard.$eval('.css-i260wg', el => el.innerHTML);
+        const priceOriginal = await productCard.$eval('.css-31z3ik.css-ndethb', el => el.innerHTML);
+        const picture = await productCard.$eval('picture > img', el => el.getAttribute('src'));
+        const href = await productCard.$eval('.product-card__link-overlay', el => el.getAttribute('href'));
+        products.push({ name, priceReduced, priceOriginal, picture, href });
+      }
+      const path = process.env.NIKE_SALE_PATH;
+      ensureDirSync(path);
+      const directories = readdirSync(path);
+      const exceptTop5Files = directories.slice(0, directories.length - 5);
+      exceptTop5Files.forEach(element => {
+        remove(path + '/' + element);
+      });
+      writeJSON(path + '/' + new Date().getTime() + '.json', products);
+
+      Logger.log('Done crawl Nike Sale Job at ' + new Date() + ` - ${atTimes}`);
+      await browser.close();
+    });
+  });
+
   /**
    * createDocument(application, configurationOptions, extraOptions);
    *
@@ -63,7 +144,7 @@ async function bootstrap() {
 
   const authDocument = SwaggerModule.createDocument(app, options, {
     include: [AuthModule, UserModule, ProfileModule, CategoryModule, ProductModule, AddressModule,
-       SwatchModule, ProductDetailModule, StockModule, OrderModule, OrderDetailModule, InvoiceModule],
+      SwatchModule, ProductDetailModule, StockModule, OrderModule, OrderDetailModule, InvoiceModule],
   });
   SwaggerModule.setup('api', app, authDocument);
 
